8f9aa695e83bcbfb8dccd1dfe3ef22c38680be36
Dayane <dayanetm@outlook.com>
2024-01-20 09:13:45 -0300

    logica-hackerrank

---
+++ findMedian.js
@@ -0,0 +1,12 @@
+// Dado um array de tamanho n, retornar esse array ordenado e o número do meio (mediana)
+
+function findMedian(arr) {
+
+    arr.sort((a,b) => a-b)
+    let middle = Math.floor(arr.length/2)
+    return arr[middle]
+
+}
+
+let medium = findMedian([0, 1, 2, 4, 6, 5, 3]);
+console.log(medium)
